d8930125864ee34c9ce3bddcb62d360e07cdc5b8
Amy DePalma <amydepalma@gmail.com>
2026-03-16 23:30:25 -0400

    Updated footer to include pete

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 </main>
 </div>
 
-<footer id="footer-secondary">
-	<div class="has-text-align-center">
-		<p class="fw-bold mt-0 w-100" style="font-size: 13.5px;line-height:1.5;margin-bottom:6px">Special appearance by none other than Pete!</p>
+<footer id="site-footer" class="has-text-align-center">
+	<div class="d-inline-flex flex-nowrap align-items-center justify-content-center gap-xs">
+		<img src="<?= get_template_directory_uri(); ?>/assets/images/pete.webp" width="28" alt="Pete" style="border-radius: 100%;">
+		<p>Special appearance by none other than Pete!</p>
 	</div>
 	</div>
 </footer>
