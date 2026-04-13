d53879a7648a511d4ba61c0115e547042b3a298e
Amy DePalma <amydepalma@gmail.com>
2026-04-13 13:37:57 -0400

    Added new frontpage

--- front-page.php
+++ front-page.php
@@ -1,12 +1,19 @@
-<?php get_header( 'std' ); ?>
+<?php
+/**
+ * Template Name:  New Frontpage
+ **/
+get_header(); ?>
+
 
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) :
 		the_post(); ?>
 		<div id="block-editor-content" class="page grid-page">
-			<?php the_content(); ?>
+			<div class="my-xxl">
+				<?php the_content(); ?>
+			</div>
 		</div>
 	<?php endwhile; ?>
 <?php endif; ?>
 
-<?php get_footer( 'std' ); ?>
+<?php get_footer(); ?>
